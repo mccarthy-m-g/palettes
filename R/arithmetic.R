8723732a30ecbbf87c3febafcb5d71ae4ab293dd
Michael McCarthy <51542091+mccarthy-m-g@users.noreply.github.com>
2021-10-30 15:21:42 -0600

    remove old code causing errors

--- R/arithmetic.R
+++ R/arithmetic.R
@@ -9,31 +9,32 @@ vec_arith.palettes_color <- function(op, x, y, ...) {
 vec_arith.palettes_color.palettes_color <- function(op, x, y, ...) {
   switch(
     op,
-    "+" = color_plus(x, y, names_common(x, y)),
-    "-" = color_minus(x, y, names_common(x, y)),
+    "+" = color_plus(x, y),
+    # TODO: Add other arithmetic operator methods
+    # "-" = color_minus(x, y),
     stop_incompatible_op(op, x, y)
   )
 }
 
-color_plus <- function(x, y, names) {
-  color_arith(x, y, names, color_plus_prismatic)
+color_plus <- function(x, y) {
+  color_arith(x, y, color_plus_prismatic)
 }
-color_minus <- function(x, y, names) {
-  color_arith(x, y, names, color_minus_farver)
+color_minus <- function(x, y) {
+  color_arith(x, y, color_minus_farver)
 }
 
-color_arith <- function(x, y, names, fn) {
+color_arith <- function(x, y, fn) {
   if (!is_color(x) || !is_color(y)) {
     abort("`x` and `y` must both be 'color' objects.")
   }
 
   args <- vec_cast_common(x = x, y = y)
-  args <- vec_recycle_common(!!!args, names = names)
+  args <- vec_recycle_common(!!!args)
   x <- args[[1]]
   y <- args[[2]]
-  names <- args$names
 
-  # TODO: Restore NAs in output
+  # TODO: Restore NAs in output. Adding to an NA should return NA.
+  # TODO: Replace with purrr
   fields <- vapply(
     seq_along(x),
     function(i) {
@@ -43,10 +44,10 @@ color_arith <- function(x, y, names, fn) {
   )
 
   color(fields)
-
-  #new_color_from_fields(fields, names)
 }
 
+# TODO: Decide the function for adding colours; the current is just a proof of
+# concept placeholder.
 color_plus_prismatic <- function(x, y) {
   prismatic::clr_mix(x, y)
 }
