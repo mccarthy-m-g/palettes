145a6a01138dab44ad3674f7e3b37bb9f4def628
Michael McCarthy <51542091+mccarthy-m-g@users.noreply.github.com>
2021-10-30 13:06:35 -0600

    update namespace imports

--- R/palettes-package.R
+++ R/palettes-package.R
@@ -1,6 +1,8 @@
 #' @keywords internal
 "_PACKAGE"
 
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
 ## usethis namespace: start
 #' @importFrom glue glue
 #' @importFrom grDevices col2rgb
@@ -13,4 +15,11 @@ NULL
 #' @import vctrs
 #' @keywords internal
 #' @name palettes-vctrs
+NULL
+
+#' Internal rlang methods
+#'
+#' @import rlang
+#' @keywords internal
+#' @name palettes-rlang
 NULL
