2a268730a6e859d8c249cbb1d757573047e69b0c
Michael McCarthy <51542091+mccarthy-m-g@users.noreply.github.com>
2022-10-14 17:13:44 -0700

    tidy up

--- R/plot.R
+++ R/plot.R
@@ -1,4 +1,5 @@
 # TODO: Decide on swatch appearance
+# TODO: Add accessibility check functions
 #' @export
 plot.palettes_color <- function(x, ...) {
   colorspace::swatchplot(x, ..., off = 0)
--- inst/interactive.R
+++ inst/interactive.R
@@ -1,7 +1,9 @@
 x <- color(c("red", "#A2A0B8FF", NA, "gray62"))
+x
 y <- color(c("red", "green", "blue"))
 xy <- palette(v1 = x, v2 = y)
 xy
+tibble::tibble(color(c("red", "#ffffff", NA)))
 
 # Class ----
 class(x)
@@ -56,45 +58,3 @@ cat(pretty_color(x + rev(x)))
 plot(x)
 
 plot(xy)
-
-# -----------------------------------------------------------------------------
-
-# Mock-up print method. The challenge is in adding a line number to the output;
-# it also does not handle wrapping well when there are lots of colours.
-x_format <- pretty_color(format(x))
-cat(
-  glue("<{vctrs::vec_ptype_full(x)}[{length(x)}]>"),
-  "\n", "[1] ",
-  paste(x_format, collapse = " "),
-  sep = ""
-)
-cat(
-  glue("<{vctrs::vec_ptype_full(x)}[{length(x)}]>"),
-  "\n", "[1] ",
-  paste(rep(x_format, 5), collapse = " "),
-  sep = ""
-)
-cat(
-  glue("<{vctrs::vec_ptype_full(x)}[{length(x)}]>"),
-  "\n", "[1] ",
-  stringr::str_wrap(paste(rep(x_format, 5), collapse = " ")),
-  sep = ""
-)
-
-cat(
-  glue("<{vctrs::vec_ptype_full(x)}[{length(x)}]>"),
-  strwrap(paste(rep(x_format, 5), collapse = " "), width = 250, prefix = c("[1] ")),
-  sep = ""
-)
-
-# This gives line numbers rather than the index of the item at the start of each line
-wrap_x <- stringr::str_wrap(paste(rep(x_format, 5), collapse = " "))
-split_x <- unlist(stringr::str_split(wrap_x, "\n"))
-glue_x <- glue("[{1:length(split_x)}] {split_x}")
-
-cat(
-  glue("<{vctrs::vec_ptype_full(x)}[{length(x)}]>"),
-  glue("[{1:length(split_x)}] {split_x}", .sep = "AHHHHHHH"),
-  sep = "\n"
-)
-
