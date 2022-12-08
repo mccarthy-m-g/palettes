39b9698660422346bcaf6b88059364648aa6ce59
Michael McCarthy <51542091+mccarthy-m-g@users.noreply.github.com>
2022-12-08 14:51:54 -0800

    add FIXME note

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,7 @@
 rgb2col <- function(x, alpha = FALSE) {
   if (alpha) {
+    # FIXME: This is never used since the alpha argument in format() is hardcoded
+    # to FALSE.
     rgb(x[1, ], x[2, ], x[3, ], alpha = x[4, ], maxColorValue = 255)
   } else {
     rgb(x[1, ], x[2, ], x[3, ], maxColorValue = 255)
