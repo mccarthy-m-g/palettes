664877c9d8fdb8160567102a968f01d989021be8
Michael McCarthy <51542091+mccarthy-m-g@users.noreply.github.com>
2022-12-12 16:06:14 -0800

    move grDevices to suggests

--- R/palettes-package.R
+++ R/palettes-package.R
@@ -4,7 +4,6 @@
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
-#' @importFrom grDevices col2rgb rgb
 #' @importFrom tibble tibble as_tibble
 #' @importFrom methods setOldClass
 ## usethis namespace: end
