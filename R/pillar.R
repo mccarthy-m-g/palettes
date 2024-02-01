8f7f19ad61ac5cf23822fa583e902dd9c2cebe71
Michael McCarthy <51542091+mccarthy-m-g@users.noreply.github.com>
2024-02-01 00:40:40 -0800

    ensure hex codes are always printed in tibbles

--- R/pillar.R
+++ R/pillar.R
@@ -1,5 +1,8 @@
 #' @importFrom pillar pillar_shaft
 #' @export
 pillar_shaft.palettes_colour <- function(x, ...) {
+  # Hex codes should always be printed in tibbles.
+  op <- options(palettes.print_hex = TRUE)
+  on.exit(options(op))
   pillar::new_pillar_shaft_simple(colour_format_symbol(x))
 }
