9d56b0ad9eb5f5229b1e082c9c5c9cf6d6164d12
Michael McCarthy <51542091+mccarthy-m-g@users.noreply.github.com>
2024-02-02 01:25:42 -0800

    manually arrange rows for consistency across printing options and edge cases

--- R/print.R
+++ R/print.R
@@ -7,9 +7,10 @@ obj_print_data.palettes_colour <- function(x, ...) {
   print_index <- getOption("palettes.print_index")
 
   out <- colour_format_symbol(x)
-  out_nchar <- max(cli::utf8_nchar(out, type = "bytes")) + nchar(print_sep)
+  out_nchar <- max(cli::ansi_nchar(out, type = "width")) + nchar(print_sep)
   out_width <- print_width * out_nchar
 
+  index <- NULL
   if (print_index) {
     index <- paste0("[", seq(from = 1, to = vec_size(x), by = print_width), "]")
     index_nchar <- max(nchar(index))
@@ -17,13 +18,20 @@ obj_print_data.palettes_colour <- function(x, ...) {
     out_width <- out_width + index_nchar + 1
   }
 
-  cat(
-    out,
-    sep = print_sep,
-    fill = out_width,
-    labels = switch(print_index + 1, NULL, index)
+  # Formatting each row manually ensures that the the maximum number of colours
+  # on a line and their indexes will always be correct; otherwise, when letting
+  # cat() determine what's on a line, there are edge cases when there are NAs in
+  # the colour vector or formatting is disabled where the maximum number of
+  # colours on a line isn't respected. Likewise, although cli::ansi_columns()
+  # performs a similar function to the code below, it has some quirks that make
+  # it unsuitable here (such as putting a separator after the last item).
+  out <- split(out, ceiling(seq_along(out)/print_width))
+  out <- purrr::map_chr(out, paste0, collapse = print_sep)
+  out[1:vec_size(out) - 1] <- purrr::map_chr(
+    out[1:vec_size(out) - 1], function(.x) paste0(.x, print_sep)
   )
 
+  cat(out, sep = "", fill = out_width, labels = index)
   invisible(x)
 }
 
