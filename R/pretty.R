c1c6254206ce085a20f5fee208086b043d7f6aaa
Michael McCarthy <51542091+mccarthy-m-g@users.noreply.github.com>
2024-02-02 01:46:16 -0800

    let `cli::ansi_align()` handle alignment

--- R/pretty.R
+++ R/pretty.R
@@ -4,23 +4,25 @@ colour_symbol <- function(x) {
 }
 
 colour_format_symbol <- function(x, ...) {
-
   print_symbol <- !isFALSE(getOption("palettes.print_symbol"))
   print_hex    <- isTRUE(getOption("palettes.print_hex"))
 
-  purrr::map_chr(
+  out <- purrr::map_chr(
     x,
     function(.x) {
       colour_index <- !is.na(.x)
       if (print_hex && print_symbol) {
-        ifelse(colour_index, paste(colour_symbol(.x), format(.x)), "     <NA>")
+        ifelse(colour_index, paste(colour_symbol(.x), format(.x)), "<NA>")
       } else if (!print_hex && print_symbol) {
         ifelse(colour_index, colour_symbol(.x), "<NA>")
       } else if (print_hex && !print_symbol) {
-        ifelse(colour_index, format(.x), "   <NA>")
+        ifelse(colour_index, format(.x), "<NA>")
       } else {
         ifelse(colour_index, paste0("\"", .x, "\""), "NA")
       }
     }
   )
+
+  out_width <- max(cli::ansi_nchar(out, type = "width"))
+  cli::ansi_align(out, width = out_width, align = "right", type = "width")
 }
